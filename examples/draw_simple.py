b770c81fd7d607fa9ca94cabd87adf55c5b6acb8
Austin Gill <notgnoshi@gmail.com>
2018-02-18 14:19:25 -0700

    fix test discovery on doctests

--- examples/draw_simple.py
+++ examples/draw_simple.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+"""
+    An example of drawing a simple grid.
+"""
+
+import sys
+sys.path.append('..')
+
 from hexgrid import Grid, DrawGrid
+from hexgrid import FLAT, OFFSET
 
 
 def main():
-    grid = Grid(coordinate_system='offset', hexagon_type='flat-topped')
+    grid = Grid(coordinate_system=OFFSET, hexagon_type=FLAT)
 
     grid[-1, -1] = None
     grid[0, 0] = None
@@ -13,11 +21,9 @@ def main():
     grid[1, 2] = None
 
     neighbors = grid.neighbor_coordinates((0, 0))
-    # neighbors = [grid.convert(c, grid.coordinate_system, 'cube') for c in neighbors]
 
     line = grid.line_coordinates((0, 0), (4, 4), validate=False)
 
-    # grid.set_coordinate_system('cube')
     draw = DrawGrid(grid)
     draw.draw_hexagons(grid.keys(), labels=True, fill='#7070ff')
     draw.draw_hexagons(neighbors, labels=True, fill='#ff7070')
