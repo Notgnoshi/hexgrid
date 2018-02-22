79091bd9e401bf61a618914329018f6636cdbd96
Austin Gill <notgnoshi@gmail.com>
2018-02-21 18:08:57 -0700

    Make shortest path drawing slightly more interesting

--- examples/draw_path.py
+++ examples/draw_path.py
@@ -6,22 +6,31 @@
 import sys
 sys.path.append('..')
 
-from hexgrid import Grid, DrawGrid
+from hexgrid import Grid, DrawGrid, HexagonType, CoordinateSystem
 from hexgrid import FLAT, OFFSET
 
 
 def main():
-    grid = Grid(coordinate_system=OFFSET, hexagon_type=FLAT)
+    # grid = Grid(coordinate_system=OFFSET, hexagon_type=FLAT)
 
-    grid[-1, -1] = None
-    grid[-1, 0] = None
-    grid[0, 0] = None
-    grid[0, 1] = None
-    grid[1, 0] = None
-    grid[1, 1] = None
-    grid[1, 2] = None
-    path = grid.shortest_path_coordinates((-1, -1), (1, 2))
-    print(path)
+    # grid[-1, -1] = None
+    # grid[-1, 0] = None
+    # grid[0, 0] = None
+    # grid[0, 1] = None
+    # grid[1, 0] = None
+    # grid[1, 1] = None
+    # grid[1, 2] = None
+    # path = grid.shortest_path_coordinates((-1, -1), (1, 2))
+    # print(path)
+
+    grid = Grid(hexagon_type=HexagonType.FLAT, coordinate_system=CoordinateSystem.CUBIC)
+
+    for i in range(-3, 4):
+        for j in range(-3, 4):
+            c = Grid.convert((i, j), CoordinateSystem.AXIAL, CoordinateSystem.CUBIC)
+            grid[c] = None
+
+    path = grid.shortest_path_coordinates((-3, 2, 1), (3, -2, -1))
 
     draw = DrawGrid(grid)
     draw.draw_hexagons(grid.keys(), labels=True, fill='#7070ff')
